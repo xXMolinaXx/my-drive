9c99da5cabd89590ce153f0f9dd9608c5715fc01
Kenny Molina <kjmolina@live.com>
2024-06-26 12:35:07 -0600

    update

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
       },
       {
         protocol: 'http',
-        hostname: 'http://18.118.8.253/',
+        hostname: '18.118.8.253',
         pathname: '/files/getFile/**',
       },
     ],
