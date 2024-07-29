0484d8630f73141b475bb09f5f13d2199a38963d
kenny molina <kenny.molina.murillo@gmail.com>
2024-07-29 13:46:24 -0600

    update

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,4 +1,9 @@
 import type { Metadata } from "next";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 
