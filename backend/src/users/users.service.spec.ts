5ae47085c2146cd28aac8cf042f1464ca26677c7
kenny molina <kenny.molina.murillo@gmail.com>
2025-01-08 21:53:34 +0000

    add user service test

---
+++ backend/src/users/users.service.spec.ts
@@ -0,0 +1,66 @@
+
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { ApiKeyGuard } from '../auth/guards/api-key.guard';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+describe('UsersController', () => {
+  let usersController: UsersController;
+  const mockUsersService = {
+    create: jest.fn().mockImplementation((dto) => {}),
+    update: jest.fn().mockImplementation((dto) => {}),
+  };
+  const mockRoleGuard = { canActivate: true };
+  const mockApiKeyGuard = { canActivate: true };
+  const userCreation: CreateUserDto = {
+    fullName: 'test',
+    email: '',
+    telphone: '',
+    DNI: '',
+    bornAt: new Date(),
+    gender: 'male',
+    password: '123',
+  };
+  const userUpdate: UpdateUserDto = {
+    fullName: 'test',
+    email: '',
+    telphone: '',
+    DNI: '',
+    bornAt: new Date(),
+    gender: 'male',
+    password: '123',
+    hashPassword: '123',
+  };
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UsersController],
+      providers: [UsersService],
+    })
+      .overrideGuard(RolesGuard)
+      .useValue(mockRoleGuard)
+      .overrideGuard(ApiKeyGuard)
+      .useValue(mockApiKeyGuard)
+      .overrideProvider(UsersService)
+      .useValue(mockUsersService)
+      .compile();
+    usersController = module.get<UsersController>(UsersController);
+  });
+
+  it('should be defined', () => {
+    expect(usersController).toBeDefined();
+  });
+  it('create user', async () => {
+    const result = await usersController.create(userCreation);
+    expect(result).toEqual({ message: 'Usuario Creado', statusCode: 200, success: true });
+  });
+  it('should read an user', async () => {
+    expect(1).toBe(1);
+  });
+  it('should update an user password', async () => {
+    const result = await usersController.update('123', userUpdate);
+    expect(result).toEqual({ message: 'Contraseña actualizada', statusCode: 200, success: true });
+  });
+});
