8481033ab8f3b4881013ea1bf07679bdee503bd7
kenny molina <kenny.molina.murillo@gmail.com>
2025-01-08 20:10:16 +0000

    test: :white_check_mark: rename users.controller.spec.ts and add create user test

--- backend/src/users/users.controller.spec.ts
+++ backend/src/users/users.controller.spec.ts
@@ -4,12 +4,24 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { ApiKeyGuard } from '../auth/guards/api-key.guard';
+import { CreateUserDto } from './dto/create-user.dto';
 
 describe('UsersController', () => {
   let usersController: UsersController;
-  const mockUsersService = {};
+  const mockUsersService = {
+    create: jest.fn().mockImplementation((dto) => {}),
+  };
   const mockRoleGuard = { canActivate: true };
   const mockApiKeyGuard = { canActivate: true };
+  const userCreation: CreateUserDto = {
+    fullName: 'test',
+    email: '',
+    telphone: '',
+    DNI: '',
+    bornAt: new Date(),
+    gender: 'male',
+    password: '123',
+  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
@@ -27,5 +39,9 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(usersController).toBeDefined();
+  });
+  it('create user', async () => {
+    const result = await usersController.create(userCreation);
+    expect(result).toEqual({ message: 'Usuario Creado', statusCode: 200, success: true });
   });
 });
