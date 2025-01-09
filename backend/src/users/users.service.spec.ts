7c0ca70caa1c3cdb018e23af964e359631ab44b3
kenny molina <kenny.molina.murillo@gmail.com>
2025-01-09 00:43:31 +0000

    update

--- backend/src/users/users.service.spec.ts
+++ backend/src/users/users.service.spec.ts
@@ -3,9 +3,25 @@ import { User } from './schemas/users.schemas';
 import { UsersService } from './users.service';
 import { getModelToken } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { CreateUserDto } from './dto/create-user.dto';
+import { find } from 'rxjs';
 
 describe('UsersService', () => {
-  const mockUserModel = {};
+  const userCreation: CreateUserDto = {
+    fullName: 'test',
+    email: '',
+    telphone: '',
+    DNI: '',
+    bornAt: new Date(),
+    gender: 'male',
+    password: '123',
+  };
+
+  const mockUserModel = {
+    findOne: jest.fn().mockImplementation(() => {}),
+    find: jest.fn().mockImplementation(() => ({ countDocuments: jest.fn().mockImplementation(() => []) })),
+    save: jest.fn().mockImplementation(() => {}),
+  };
   let usersService: UsersService;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -29,5 +45,9 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(usersService).toBeDefined();
+  });
+  it('should create user', async () => {
+    const response = await usersService.create(userCreation);
+    expect(response).toBeUndefined();
   });
 });
