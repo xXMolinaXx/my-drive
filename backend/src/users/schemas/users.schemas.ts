9c02c06317188e0ac5b47eea0f8621f723206cbd
kenny molina <kenny.molina.murillo@gmail.com>
2025-01-10 18:06:46 +0000

    update user schema

--- backend/src/users/schemas/users.schemas.ts
+++ backend/src/users/schemas/users.schemas.ts
@@ -14,24 +14,10 @@ export class User {
   fullName: string;
   @Prop({ required: true })
   email: string;
-  @Prop({ required: true, default: '1' })
-  telphone: string;
-  @Prop({ required: false, default: '1' })
-  DNI: string;
-  @Prop({ required: true, default: new Date() })
-  bornAt: Date;
-  @Prop({ required: true, enum: ['hombre', 'mujer'], default: 'hombre' })
-  gender: string;
   @Prop({ required: true })
   password: string;
-  @Prop({ default: 'client', type: String, enum: ['client', 'admin', 'flebotomista'], required: true })
-  role: string;
-  @Prop({ default: 'ninguno', enum: ['ninguno', 'la granja', 'tepeyac', 'aeroplaza'], type: String })
-  store: string;
   @Prop()
   resetPassword: resetPassword;
-  @Prop({ type: Number, require: true, default: 1900 })
-  yearBorn: number;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
